f97c60dee59ee844462d7b92c46ba186c1c13e96
thdau <info@frentree.com>
2023-04-20 11:10:07 +0900

    vo push

--- skt_pcAction2/src/com/recon/util/database/ibatis/vo/remediateActionVo.java
+++ skt_pcAction2/src/com/recon/util/database/ibatis/vo/remediateActionVo.java
@@ -9,25 +9,42 @@ public class remediateActionVo {
 	private String target_id;
 	private int ap_no;
 	private String hostname;
+	private String name;
 	private String path_orig;
 	private String path;
+	private String hash_id;
+	private String type;
+	private String category_no;
+	private int done;
+	private int success;
+	private String result;
 	private Date regdate;
+	private String remediation_chk;
 	
 	public remediateActionVo() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public remediateActionVo(int index, int action, String target_id, int ap_no, String hostname, String path_orig,
-			String path, Date regdate) {
+	public remediateActionVo(int index, int action, String target_id, int ap_no, String hostname, String name,
+			String path_orig, String path, String hash_id, String type, String category_no, int done, int success,
+			String result, Date regdate, String remediation_chk) {
 		super();
 		this.index = index;
 		this.action = action;
 		this.target_id = target_id;
 		this.ap_no = ap_no;
 		this.hostname = hostname;
+		this.name = name;
 		this.path_orig = path_orig;
 		this.path = path;
+		this.hash_id = hash_id;
+		this.type = type;
+		this.category_no = category_no;
+		this.done = done;
+		this.success = success;
+		this.result = result;
 		this.regdate = regdate;
+		this.remediation_chk = remediation_chk;
 	}
 
 	public int getIndex() {
@@ -70,6 +87,14 @@ public class remediateActionVo {
 		this.hostname = hostname;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getPath_orig() {
 		return path_orig;
 	}
@@ -86,6 +111,54 @@ public class remediateActionVo {
 		this.path = path;
 	}
 
+	public String getHash_id() {
+		return hash_id;
+	}
+
+	public void setHash_id(String hash_id) {
+		this.hash_id = hash_id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getCategory_no() {
+		return category_no;
+	}
+
+	public void setCategory_no(String category_no) {
+		this.category_no = category_no;
+	}
+
+	public int getDone() {
+		return done;
+	}
+
+	public void setDone(int done) {
+		this.done = done;
+	}
+
+	public int getSuccess() {
+		return success;
+	}
+
+	public void setSuccess(int success) {
+		this.success = success;
+	}
+
+	public String getResult() {
+		return result;
+	}
+
+	public void setResult(String result) {
+		this.result = result;
+	}
+
 	public Date getRegdate() {
 		return regdate;
 	}
@@ -94,13 +167,21 @@ public class remediateActionVo {
 		this.regdate = regdate;
 	}
 
+	public String getRemediation_chk() {
+		return remediation_chk;
+	}
+
+	public void setRemediation_chk(String remediation_chk) {
+		this.remediation_chk = remediation_chk;
+	}
+
 	@Override
 	public String toString() {
 		return "remediateActionVo [index=" + index + ", action=" + action + ", target_id=" + target_id + ", ap_no="
-				+ ap_no + ", hostname=" + hostname + ", path_orig=" + path_orig + ", path=" + path + ", regdate="
-				+ regdate + "]";
+				+ ap_no + ", hostname=" + hostname + ", name=" + name + ", path_orig=" + path_orig + ", path=" + path
+				+ ", hash_id=" + hash_id + ", type=" + type + ", category_no=" + category_no + ", done=" + done
+				+ ", success=" + success + ", result=" + result + ", regdate=" + regdate + ", remediation_chk="
+				+ remediation_chk + "]";
 	}
-	
-	
-	
+		
 }
